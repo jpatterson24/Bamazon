550dd18b75a564ae51d110ee901376cfb58ded20
Jamal Patterson <jamal@jamals-mbp.home>
2017-12-22 22:21:54 -0500

    updated

--- schema.sql
+++ schema.sql
@@ -15,25 +15,25 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
-("Amazon Echo", "Electronics", 179.99, 20),
+("Amazon Echo", "Electronics", 179.99, 200),
 
-("Amazon Echo Dot", "Electronics", 49.99, 15),
+("Amazon Echo Dot", "Electronics", 49.99, 150),
 
-("Instant Pot 7-in-1 MultiFunctional Pressure Cooker", "Home & Kitchen", 129.95, 10),
+("Instant Pot 7-in-1 MultiFunctional Pressure Cooker", "Home & Kitchen", 129.95, 100),
 
-("Fire HD Tablets", "Electronics", 109.99, 10),
+("Fire HD Tablets", "Electronics", 109.99, 100),
 
-("SanDisk 32GB Ultra Class Memory Card", "Electronics", 13.95, 5),
+("SanDisk 32GB Ultra Class Memory Card", "Electronics", 13.95, 150),
 
-("Sony XB950B1 Extra Bass Wireless Headphones with App Control", "Electronics", 148.99, 30),
+("Sony XB950B1 Extra Bass Wireless Headphones with App Control", "Electronics", 148.99, 300),
 
-("Call of Duty WWII - Playstation 4", "Video Game", 54.95, 25),
+("Call of Duty WWII - Playstation 4", "Video Game", 54.95, 250),
 
-("EatSmart Precision Digital Bathroom Scale", "Electronics", 14.95, 10),
+("EatSmart Precision Digital Bathroom Scale", "Electronics", 14.95, 100),
 
-("iXCC Certified Lightning Cable for iPhones", "Electronics", 7.00, 15),
+("iXCC Certified Lightning Cable for iPhones", "Electronics", 7.00, 150),
 
-("Echo Show - Black", "Electronics", 149.99, 16);
+("Echo Show - Black", "Electronics", 149.99, 160);
 
 
 select * from products;
